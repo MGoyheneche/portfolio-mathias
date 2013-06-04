5836ff9a2d56495d6925e45b28433b21b2b65aea
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2013-06-04 23:55:17 +0200

    header almost done... TODO : center picture and catchword

--- index.php
+++ index.php
@@ -3,38 +3,70 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title></title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width">
 
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-        <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-        <![endif]-->
-
-				<?php echo "hello"; ?>
- 				<?php bloginfo( 'stylesheet_url' ); ?>
+		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+		<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+		<![endif]-->
 
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+		<header>
+			<div class="center">
+				<div class="top">
+					<div class="logotype">
+						<a href="#"><img src="<?php bloginfo( 'template_directory' ); ?>/images/mathias-goyheneche-logo.png" alt="Mathias Goyheneche | Designer & développeur"></a>
+					</div>
+					<nav class="primary-naviguation">
+						<ul>
+							<li><a href="/" class="current-page">A propos</a></li>
+							<li><a href="/work">Travaux</a></li>
+							<li><a href="/contact">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
 
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src='//www.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
-    </body>
+				<article class="cover-article">
+					<header>
+						<div class="cover-image">
+							<img src="<?php bloginfo( 'template_directory' ); ?>/images/mathias-goyheneche-photo.png" alt="Mathias Goyheneche | Designer & développeur" class="cover-photo">
+						</div><!--
+				 --><h1>
+							<span class="hello">Bonjour,</span>
+							<span class="who-i-am">je suis Mathias, </span>
+							<span class="what-i-do">
+								<em>designer</em>
+								<span class="fancy-ampersand">&</span>
+								<em>développeur</em> *.
+							</span>
+						</h1>
+					</header>
+				</article>
+			</div>
+
+		</header>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+		<script src="js/plugins.js"></script>
+		<script src="js/main.js"></script>
+
+		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+		<script>
+			var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+			g.src='//www.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script>
+	</body>
 </html>
